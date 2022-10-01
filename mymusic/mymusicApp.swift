9d0deeb51127c6533c598539e2e9247d2ff7bf43
akula381 <akulanvn381@gmail.com>
2022-10-01 13:52:40 -0600

    Initial Commit

---
+++ mymusic/mymusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  mymusicApp.swift
+//  mymusic
+//
+//  Created by Naveen Akkula on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct mymusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
